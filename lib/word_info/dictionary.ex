320eeba5e834ddb24badb6bc8a29b2b292cae22d
qhwa <qhwa@163.com>
2020-04-24 16:19:59 +0800

    allow setting db_dir, make it possible in escripts

--- lib/word_info/dictionary.ex
+++ lib/word_info/dictionary.ex
@@ -3,16 +3,11 @@ defmodule WordInfo.Dictionary do
 
   use Agent
 
-  def start_link(_) do
-    Agent.start_link(&load/0, name: __MODULE__)
-  end
+  def start_link(_),
+    do: Agent.start_link(&load/0, name: __MODULE__)
 
   defp load do
-    {:ok, table} =
-      Path.join(:code.priv_dir(:word_info), "merged.tab")
-      |> String.to_charlist()
-      |> :ets.file2tab()
-
+    {:ok, table} = :ets.file2tab(WordInfo.EtsData.db_file())
     table
   end
 
